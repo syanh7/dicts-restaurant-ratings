2b234d07e1c37e99b2a2cc19c763cbfd5ea3edad
sierra hughey <sierrahughey@mailbox.org>
2021-07-20 00:27:31 +0000

    finished dict functions

--- ratings.py
+++ ratings.py
@@ -2,5 +2,54 @@
 
 
 # put your code here
+# open the file
+# read the scores file - line by line and strip using : split (":")
+# create a dictionary 
+# enter the key and value into the dictionary 
+# Key into a list to sort alphabetically in the list
+# print the list /print the dictionary with an f string to include "is rated at"
+
+def restaurant_ratings_from_file(file_name):
+    #restaurant_dict = {}
+
+    file = open(file_name)
+
+    for line in file:
+        line = line.rstrip()
+        restaurant_name, rating = line.split(':')
+
+        restaurant_dict[restaurant_name] = rating
+    
+    file.close()
+    
+def restaurant_ratings_from_user_input():
+    restaurant_name = input("Enter the name of the restaurant: ")
+    rating = input("Enter the rating of the restaurant: ")
+
+    restaurant_dict[restaurant_name] = rating
+
+def print_restaurant_ratings():
+    # restaurant_name_list = restaurant_dict.keys()
+    # restaurant_name_list = sorted(restaurant_name_list)
+
+    # for key in restaurant_name_list:
+    #     print(f'{key} is rated at {restaurant_dict[key]}'))
+
+    for key in sorted(restaurant_dict):
+        print(f'{key} is rated at {restaurant_dict[key]}')
+
+
+
+restaurant_dict = {}
+restaurant_ratings_from_file('scores.txt')
+
+while True:
+    
+    restaurant_ratings_from_user_input()
+    print_restaurant_ratings()
+
+    user_input = input("Press [q] to quit, enter to continue: ").lower()
+    if user_input == 'q':
+        break
 
 
