5a22965f22b4eb7d1b9f2ad6f38e2ca33521a5fb
sierra hughey <sierrahughey@mailbox.org>
2021-07-20 00:44:16 +0000

    commenting

--- ratings.py
+++ ratings.py
@@ -10,10 +10,13 @@
 # print the list /print the dictionary with an f string to include "is rated at"
 
 def restaurant_ratings_from_file(file_name):
-    #restaurant_dict = {}
-
+    #open file
     file = open(file_name)
 
+    #goes through each line of file
+    #split the line by ':' and unpack
+    #into two variables
+    #add key and value to dictonairy 
     for line in file:
         line = line.rstrip()
         restaurant_name, rating = line.split(':')
@@ -23,9 +26,11 @@ def restaurant_ratings_from_file(file_name):
     file.close()
     
 def restaurant_ratings_from_user_input():
+    #get user input for resturant and rating
     restaurant_name = input("Enter the name of the restaurant: ")
     rating = input("Enter the rating of the restaurant: ")
 
+    #add user generated resturant and rating to dict 
     restaurant_dict[restaurant_name] = rating
 
 def print_restaurant_ratings():
@@ -35,14 +40,17 @@ def print_restaurant_ratings():
     # for key in restaurant_name_list:
     #     print(f'{key} is rated at {restaurant_dict[key]}'))
 
+    #gets a sorted list of keys from restaurant_dict
+    #iterates through the list, gets value by key from restaurant dict
     for key in sorted(restaurant_dict):
         print(f'{key} is rated at {restaurant_dict[key]}')
 
 
-
+#creates global dict restaurant_dict
 restaurant_dict = {}
 restaurant_ratings_from_file('scores.txt')
 
+#loops through user input
 while True:
     
     restaurant_ratings_from_user_input()
